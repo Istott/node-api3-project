f990245e7813afda927efd5ad7ff99275291c310
isaac stott <isaac.cloyd@gmail.com>
2020-06-04 14:11:36 -0600

    added env info to server.get

--- server.js
+++ server.js
@@ -40,7 +40,11 @@ server.use("/api/posts", postRouter);
 server.use("/api/users", userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).json({ 
+    environment: process.env.NODE_ENV, 
+    port: process.env.PORT,
+    greeting: process.env.GREET
+});
 });
 
 //custom middleware
