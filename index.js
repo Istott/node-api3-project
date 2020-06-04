997ec7ec3a07b7c2090191c9845348f905b546c1
isaac stott <isaac.cloyd@gmail.com>
2020-06-04 14:09:35 -0600

    ready to deploy

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 // code away!
-// const server = require("./server.js");
-
-// const port = 4000;
-// server.listen(port, () => {
-//   console.log(`\n== Server Running on http://localhost:${port} ==\n`);
-// });
-
-
 require('dotenv').config();
 const server = require("./server.js");
 
